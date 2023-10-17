d91854977f171ef98cffaee517daad8d944f3a75
Ally Santana <136754520+ally-tpfe@users.noreply.github.com>
2023-10-17 19:59:27 -0300

    ++

--- produtos/02-customize/02. DESENVOLVIMENTO/customize-prod/src/components/modules/NavigationToggle.tsx
+++ produtos/02-customize/02. DESENVOLVIMENTO/customize-prod/src/components/modules/NavigationToggle.tsx
@@ -5,12 +5,12 @@ import React from 'react'
 export default function NavigationToggle() {
   return (
     <div className="mb-2 flex h-8 w-20 items-center justify-between gap-3">
-      <span className="flex h-full w-full items-center justify-center rounded-full bg-customize-nav-button-transparent-white text-xl font-bold text-white  shadow-customize-title-card-shadow">
+      <a
+        href="/start"
+        className="flex h-full w-full items-center justify-center rounded-full bg-customize-nav-button-transparent-white text-xl font-bold text-white shadow-customize-title-card-shadow  hover:cursor-pointer"
+      >
         <CaretLeft weight="bold" />
-      </span>
-      <span className="flex h-full w-full items-center justify-center rounded-full bg-customize-nav-button-transparent-white text-xl font-bold text-white  shadow-customize-title-card-shadow">
-        <CaretRight weight="bold" />
-      </span>
+      </a>
     </div>
   )
 }
